5fd6e30cb2491dfcb8b58b4f24fa140995649bcf
JozeSIMAO <jozesimao10@gmail.com>
2023-06-28 02:29:27 -0700

    test_on_main.c

---
+++ main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    _printf("%s\n", "This is printf");
+    _printf("%d\n", 123456789);
+    _printf("%i\n", 246810);
+    _printf("%c\n", 'A');
+    _printf("%%\n");
+    return (0);
+}
